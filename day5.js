cffe40026c753458cfb9a774a6b859168f76b01d
spsarras <psarras.st@gmail.com>
2023-12-05 08:04:56 +0000

    parsing the data

---
+++ day5.js
@@ -0,0 +1,52 @@
+﻿import {readIteratorAsArray} from "./bot.js";
+
+const path = 'day5.input.txt';
+let lines = await readIteratorAsArray(path);
+
+function extractNumbers(line) {
+    let numbers = [];
+    let seedsStrings = line.split(" ");
+    for (const seedsString of seedsStrings) {
+        let number = parseInt(seedsString.trim());
+        if (!isNaN(number))
+            numbers.push(parseInt(seedsString.trim()));
+    }
+    return numbers;
+}
+
+export function processFile(lines) {
+    let data = {};
+    let seeds = [];
+    let maps = {};
+    let i = 0;
+    let currentMap = "";
+    let keyOfMaps = [
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location"
+    ];
+    for (const line of lines) {
+        if(line.trim() === "")
+            continue;
+        if (line.startsWith("seeds:")) {
+            let seedsLine = line.substring("seeds:".length);
+            seeds.push(...extractNumbers(seedsLine));
+        } else {
+            if (currentMap !== "")
+                maps[currentMap].push(extractNumbers(line));
+        }
+        for (const key of keyOfMaps) {
+            if (line.startsWith(key)) {
+                currentMap = key;
+                maps[currentMap] = [];
+            }
+        }
+    }
+    data.seeds = seeds;
+    data.maps = maps;
+    return data;
+}
---
+++ day5.test.js
@@ -0,0 +1,24 @@
+﻿import assert from 'node:assert';
+import {test} from 'node:test';
+import {processFile} from "./day5.js";
+
+
+test('get the seeds', (t) => {
+    let lines = [
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        " 37 52 2",
+        "39 0 15",];
+    let data = processFile(lines);
+    console.log(JSON.stringify(data));
+    let seeds = data.seeds;
+    assert.strictEqual(seeds.length, 4);
+    assert.strictEqual(seeds[0], 79);
+});
+
