66327a5511c5660d77432dbfcf0e48e9ee387a99
spsarras <psarras.st@gmail.com>
2023-12-05 23:38:07 +0000

    finished day4

---
+++ day4.js
@@ -0,0 +1,78 @@
+﻿import {readIteratorAsArray} from "./bot.js";
+import {fileURLToPath} from "url";
+
+export function CalcCard(line) {
+    let data = {};
+    let card = line.split(":");
+    let numberComponents = card[0].split(" ").filter(x => x !== "");
+    data.cardNumber = parseInt(numberComponents[1]);
+    if (isNaN(data.cardNumber))
+        console.log(`cardNumber:${card[0]} ${numberComponents[1]}`)
+    let scratchCard = card[1].split("|");
+    data.winningNumbers = scratchCard[0].split(" ")
+        .map(x => parseInt(x))
+        .filter(x => !isNaN(x));
+    data.scratchNumbers = scratchCard[1].split(" ")
+        .map(x => parseInt(x))
+        .filter(x => !isNaN(x));
+    let value = 0;
+    data.wins = 0;
+    // console.log(`winningNumbers: ${winningNumbers}`);
+    // console.log(`scratchNumbers: ${scratchNumbers}`);
+    for (const winningNumber of data.winningNumbers) {
+        if (data.scratchNumbers.includes(winningNumber)) {
+            if (value === 0) value = 1; else value *= 2;
+            data.wins++;
+        }
+    }
+    return [value, data];
+}
+
+export function SumOfCardValues(lines) {
+    let data = {};
+    let sum = 0;
+    for (const line of lines) {
+        let [value, d] = CalcCard(line);
+        data[d.cardNumber] = d;
+        sum += value;
+    }
+    return [sum, data];
+}
+
+async function dayOne(path) {
+    let lines = await readIteratorAsArray(path);
+    let sum = SumOfCardValues(lines);
+    console.log(`Day 1: ${sum}`);
+}
+
+async function dayTwo(path) {
+    let lines = await readIteratorAsArray(path);
+    let sum = CountWinCards(lines);
+    console.log(`Day 2: ${sum}`);
+}
+
+export function CountWinCards(example) {
+    let [sum, data] = SumOfCardValues(example);
+    let cards = [];
+    let wins = 0;
+    for (const [key, values] of Object.entries(data).reverse()) {
+        cards.push(values);
+    }
+    wins += cards.length;
+    while (cards.length > 0) {
+        let card = cards.pop();
+        if (card.wins === 0) continue;
+        wins += card.wins;
+        for (let i = 0; i < card.wins; i++) {
+            let index = 1 + i + card.cardNumber;
+            cards.push(data[index]);
+        }
+    }
+
+    return wins;
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    await dayOne("day4.input.txt");
+    await dayTwo("day4.input.txt");
+}
---
+++ day4.test.js
@@ -0,0 +1,29 @@
+﻿import {CalcCard, SumOfCardValues, CountWinCards} from "./day4.js";
+import assert from 'node:assert';
+import {test} from 'node:test';
+
+let example = [
+    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11  ",
+];
+
+test('value of first scratch card', (t) => {
+    let [cardValue, data] = CalcCard(example[0]);
+    console.log(JSON.stringify(data));
+    assert.strictEqual(cardValue, 8);
+});
+
+test('sum of the values of all the cards', (t) => {
+    let [cardValue, data] = SumOfCardValues(example);
+    console.log(JSON.stringify(data));
+    assert.strictEqual(cardValue, 13);
+});
+
+test('calc number of crazy winning scratchcards', (t) => {
+    let wins = CountWinCards(example);
+    assert.strictEqual(wins, 30);
+});
