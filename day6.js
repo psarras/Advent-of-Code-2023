9077e222e1858cfee00ffdfd1604163322d5960a
spsarras <psarras.st@gmail.com>
2023-12-06 14:58:53 +0000

    end of part2 day6

---
+++ day6.js
@@ -0,0 +1,46 @@
+﻿import {readIteratorAsArray} from "./bot.js";
+import {fileURLToPath} from "url";
+
+export function WinMultiples(races)
+{
+    let winConditions = [];
+    for (let i = 0; i < races.length; i++)
+    {
+        const [time, distance] = races[i];
+        winConditions.push(WinConditions(distance, time));
+    }
+
+    let multiples = 1;
+    for (let i = 0; i < winConditions.length; i++)
+    {
+        multiples *= winConditions[i];
+    }
+    return multiples;
+}
+
+export function WinConditions(distance, time)
+{
+    let betterTimes = 0;
+    for (let i = 1; i < time; i++)
+    {
+        let rate = i;
+        let distanceTraveled = (time - i) * i;
+        // console.log(`distance traveled: ${distanceTraveled}`)
+        if (distanceTraveled > distance)
+        {
+            betterTimes++;
+        }
+    }
+
+    return betterTimes;
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url))
+{
+    let races = [[41, 214], [96, 1789], [88, 1127], [94, 1055]];
+    let multiples = WinMultiples(races);
+    console.log(`Answer to Day6 ${multiples}`);
+
+    let winConditions = WinConditions(214178911271055, 41968894);
+    console.log(`Answer to Day6 ${winConditions}`);
+}
---
+++ day6.test.js
@@ -0,0 +1,24 @@
+﻿import assert from 'node:assert';
+import {test} from 'node:test';
+import {WinConditions, WinMultiples} from "./day6.js";
+
+
+test('number of ways you can win', (t) =>
+{
+    let distance = 9;
+    let time = 7;
+    let times = WinConditions(distance, time);
+    assert.strictEqual(times, 4);
+});
+
+test('multiples of ways to win', (t) =>
+{
+    let multiples = WinMultiples([[7, 9], [15, 40], [30, 200]]);
+    assert.strictEqual(multiples, 288);
+});
+
+test('master race example', (t) =>
+{
+    let multiples = WinConditions(940200, 71530);
+    assert.strictEqual(multiples, 71503);
+});
