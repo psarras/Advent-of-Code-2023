e037cb4e632d3a0e729e8f20fef43675a4993eb3
spsarras <psarras.st@gmail.com>
2023-12-07 11:26:29 +0000

    simple play combination check

---
+++ day7.js
@@ -0,0 +1,76 @@
+﻿export const compos = [
+    {combination: "Five of a kind", value: 100},
+    {combination: "Four of a kind", value: 100},
+    {combination: "Full house", value: 100},
+    {combination: "Three of a kind", value: 100},
+    {combination: "Two Pair", value: 100},
+    {combination: "One Pair", value: 100},
+    {combination: "High Card", value: 100},
+]
+
+function ProcessPlay(play)
+{
+    let components = play.split(" ");
+    return {cards: components[0], bet: parseInt(components[1])};
+}
+
+export function CalcCompos(plays)
+{
+    let compos = [];
+    for (let i = 0; i < plays.length; i++)
+    {
+        compos.push(CalcCompo(plays[i]));
+    }
+    return compos;
+}
+
+export function CalcCompo(play)
+{
+    let {cards, bet} = ProcessPlay(play);
+    let cardCount = {};
+    for (let i = 0; i < cards.length; i++)
+    {
+        let card = cards[i];
+        if (!cardCount[card])
+        {
+            cardCount[card] = 0;
+        }
+        cardCount[card]++;
+    }
+    let entries = Object.entries(cardCount).sort(([, a], [, b]) => b - a);
+    console.log(entries);
+    for (let i = 0; i < entries.length; i++)
+    {
+        const [card, count] = entries[i];
+        switch (count)
+        {
+            case 5:
+                return compos[0];
+            case 4:
+                return compos[1];
+            case 3:
+            {
+                let nextBest = entries[i + 1]
+                let [card2, count2] = nextBest;
+                if (count2 === 2)
+                    return compos[2];
+                else
+                    return compos[3];
+            }
+            case 2:
+            {
+
+                let nextBest = entries[i + 1]
+                let [card2, count2] = nextBest;
+                if (count2 === 2)
+                    return compos[4];
+                else
+                    return compos[5];
+            }
+            case 1:
+                return compos[6];
+        }
+    }
+
+    return compos[0];
+}
---
+++ day7.test.js
@@ -0,0 +1,34 @@
+﻿import assert from 'node:assert';
+import {test} from 'node:test';
+import {CalcCompo, CalcCompos, compos} from "./day7.js";
+
+const examplePlays = [
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483",
+];
+
+// test('Calculate the total winnings on the example', (t) =>
+// {
+//     let winnings = CalcWinnings(examplePlays);
+//     assert.strictEqual(winnings, 6440);
+// });
+
+test('Get the Compo of one play', (t) =>
+{
+    let play = examplePlays[2];
+    let compo = CalcCompo(play);
+    assert.strictEqual(compo.combination, "Two Pair");
+});
+
+test('Get All the compos', (t) =>
+{
+    let compo = CalcCompos(examplePlays);
+    assert.strictEqual(compo[0].combination, compos[5].combination);
+    assert.strictEqual(compo[1].combination, compos[3].combination);
+    assert.strictEqual(compo[2].combination, compos[4].combination);
+    assert.strictEqual(compo[3].combination, compos[4].combination);
+    assert.strictEqual(compo[4].combination, compos[3].combination);
+});
