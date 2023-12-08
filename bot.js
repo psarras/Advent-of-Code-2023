9bb345510e73b0ac445d91cffee498d6d65afa03
spsarras <psarras.st@gmail.com>
2023-12-08 15:49:41 +0000

    re org of bot.js

--- bot.js
+++ bot.js
@@ -1,62 +1,51 @@
 import {readFileSync} from 'fs';
-
-// const lineReader = require('line-reader');
-
-let path = 'test.txt';
+import {open} from 'node:fs/promises';
+import {fileURLToPath} from "url";
 
 /**
  * @param path - path to file
  */
-function readAll(path)
-{
+function readAll(path) {
     let data = readFileSync(path);
     console.log(data.toString());
 }
 
-// readAll(path);
-
-import {open} from 'node:fs/promises';
-
-// await myFileReader(path);
-
-export async function myFileReader(path)
-{
+export async function myFileReader(path) {
     const file = await open(path);
     let i = 0;
-    for await (const line of file.readLines())
-    {
+    for await (const line of file.readLines()) {
         console.log(`${i++}: ${line}`);
     }
 }
 
-function* makeRangeIterator(start = 0, end = Infinity, step = 1)
-{
+function* makeRangeIterator(start = 0, end = Infinity, step = 1) {
     let iterationCount = 0;
-    for (let i = start; i < end; i += step)
-    {
+    for (let i = start; i < end; i += step) {
         iterationCount++;
         yield i;
     }
     return iterationCount;
 }
 
-export async function* readIterator(path)
-{
+export async function* readIterator(path) {
     const file = await open(path);
     let i = 0;
-    for await (const line of file.readLines())
-    {
+    for await (const line of file.readLines()) {
         yield line;
     }
 }
 
-export async function readIteratorAsArray(path)
-{
+export async function readIteratorAsArray(path) {
     let lines = [];
     const file = await open(path);
-    for await (const line of file.readLines())
-    {
+    for await (const line of file.readLines()) {
         lines.push(line.trim())
     }
     return lines;
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    let path = 'test.txt';
+    readAll(path);
+    await myFileReader(path);
 }
